f2904dc6d25e1a61341e136725432b0145ed3c0f
Alef Carlos <alef.carlos@gmail.com>
2019-09-24 21:48:40 -0300

    feat: try travis

---
+++ src/HelloWorld.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace PlusUltra.Nuget.HelloWorld
+{
+    public static class HelloWorld
+    {
+        public static void Write() => Write("Hellow World");
+        public static void Write(string msg) => Console.WriteLine(msg);
+
+    }
+}
