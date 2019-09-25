c6488ed9469e40c37c28794695dec25cabe24160
Alef Carlos <alef.carlos@gmail.com>
2019-09-24 23:11:12 -0300

    docs

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+echo "[INFO] Adding Nuget Source"
 dotnet nuget source Add -Name "GitHub" -Source $NUGET_SOURCE -UserName alefcarlos -Password $NUGET_API_KEY
+
+echo "[INFO] Publising.."
 
 dotnet nuget push ./pkg/PlusUltra.Nuget.HelloWorld.1.0.0.nupkg -Source "GitHub"
